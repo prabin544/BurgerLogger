b95df4fc522089d708a884184c4da631ec71e167
Prabin Singh <pravin544@gmail.com>
2020-10-02 18:48:02 -0500

    code refactor

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -27,7 +27,7 @@ $(function() {
 
     var newBurger = {
       name: $("#ca").val().trim(),
-      eaten: $("[name=eaten]:checked").val().trim()
+      //eaten: $("[name=eaten]:checked").val().trim()
     };
 
     // Send the POST request.
